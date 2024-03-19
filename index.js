e373ab165b5c40ddc72f28c6a89c9211514f909d
adityaastic <adityaastic@gmail.com>
2024-03-19 14:39:08 +0530

    add file

---
+++ index.js
@@ -0,0 +1,27 @@
+const jsonFile = require("jsonfile");
+const simpleGit = require("simple-git");
+const moment = require("moment");
+const filePath = "./data.json";
+
+const hackCommit = (n) => {
+	if (n === 0) return simpleGit().push();
+	let date = Math.floor(Math.random() * 7);
+	let week = Math.floor(Math.random() * 40) +15;
+	const DATE = moment()
+		.subtract(1, "y")
+		.add(1, "d")
+		.add(date, "d")
+		.add(week, "w")
+		.format();
+	// console.log(date, "--", week);
+	console.log(DATE);
+	const data = {
+		date: DATE,
+	};
+	jsonFile.writeFile(filePath, data, () => {
+		simpleGit()
+			.add([filePath])
+			.commit(DATE, { "--date": DATE }, hackCommit.bind(this, --n));
+	});
+};
+hackCommit(250);
